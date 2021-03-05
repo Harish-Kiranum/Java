17637b0f0b8abd864335a17753d198758ea2385f
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-05 22:05:44 +0530

    Conditional-Branching

--- Topics/Conditional -Branching/condition.java
+++ Topics/Conditional -Branching/condition.java
@@ -1,1 +1,18 @@
-
+public class condition{
+    public static void main(String args[]){
+        int num=1567;
+        if(num>=1 && num<=100){
+            System.out.println("It is a two digit number");
+        }
+        else if(num>=100 && num<=1000){
+            System.out.println("It is a three digit number");
+        }
+        else if(num>=1000 && num<=10000){
+            System.out.println("It is a four digit number");
+        }
+        else{
+            System.out.println("The number is not bertween 1 and 9999");
+        }
+        
+    }
+}
