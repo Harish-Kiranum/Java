930d3ec64366730c768eb5feb803624bc84fa8e2
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-05 22:14:09 +0530

    looping

---
+++ Topics/Looping/bee.java
@@ -0,0 +1,16 @@
+public class bee {
+    public static void main(String args[]){
+        int x=1;
+        while(x>0){
+            System.out.println("DooBee");
+            System.out.println("Bee");
+            x=x-1;
+        if(x==2){
+                System.out.println("Do");
+        }   }
+
+
+        
+    }
+    
+}
