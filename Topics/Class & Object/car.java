bd8ae1b79d0792c31647d14828a5de27c6a02734
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-09 22:03:22 +0530

    simple program on class & object

--- Topics/Class & Object/car.java
+++ Topics/Class & Object/car.java
@@ -1,1 +1,32 @@
+public class car{
+    String color;   
+    String model;
+    String brand;//instance variables
+    
 
+    
+}
+ class test{
+    public static void main(String args[]){
+        car c=new car();
+        car c1=new car();
+        car c2=new car();
+        c.color="RED";
+        c.brand="maruti";
+        c.model="swift";
+        c1.color="BLACK";
+        c1.brand="mahindra";
+        c1.model="swift";
+        c2.color="WHITE";
+        c2.brand="maruti";
+        c2.model="xuv500";
+        System.out.println(c.color+" "+c.brand+" "+c.model);
+        System.out.println(c1.color+" "+c1.brand+" "+c1.model);
+        System.out.println(c2.color+" "+c2.brand+" "+c2.model);
+        
+        
+
+        
+        
+    }
+}
