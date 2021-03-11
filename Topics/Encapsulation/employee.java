ec3558383b99cd1a3b28b37ec501c877e219d298
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-11 20:17:48 +0530

    Encapsulation real time example

--- Topics/Encapsulation/employee.java
+++ Topics/Encapsulation/employee.java
@@ -1,1 +1,43 @@
-
+class emp_details{
+    private String name;  //Instance variables
+    private int age;
+    private String company;
+    private int stipend;
+    public String getname(){ //Get method 
+        return name;
+    }
+    public int getage(){
+        return age;
+    }
+    public String getcompany(){
+        return company;
+    }
+    public int getstipend(){
+        return stipend;
+    }
+    public void setname(String n){ //set method
+        name = n;
+    }
+    public void setage(int a){
+        age = a;
+    }
+    public void setcompany(String c){
+        company = c;
+    }
+    public void setstipend(int s){
+        stipend = s;
+    }
+}
+public class employee {
+    public static void main(String args[]){
+        emp_details e= new emp_details();
+        e.setname("Harish");
+        e.setage(20);
+        e.setcompany("Kiranum private Limited");
+        e.setstipend(12000);
+        System.out.println("Employee name is"+" "+ e.getname());
+        System.out.println("Employee age is"+" "+ e.getage());
+        System.out.println("Employee company name"+" "+ e.getcompany());
+        System.out.println("Employee intern stipend"+" "+ e.getstipend());
+    }
+}
