daa10c7477c96a8272fc93984b8132556e912c0c
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-19 09:00:25 +0530

    Overloading methods

--- Topics/polymorphism/compile- time polymorphism/sports1.java
+++ Topics/polymorphism/compile- time polymorphism/sports1.java
@@ -1,1 +1,23 @@
+// method overloading in polymorphism
+class sports1{
+    public void score_details(int cricket){
+        System.out.println("cricket match score"+" "+cricket);// same name but different argument list
 
+    }
+    public void score_details(int cricket ,int football){
+        System.out.println("cricket match and football match"+" "+cricket+" "+football);
+    }
+    public void score_details(int cricket, int football, int hockey){
+        System.out.println("Cricket match, football match and hockey match"+" "+cricket+" "+football+" "+hockey);
+    }
+
+    public static void main(String args[]){
+        sports1 s= new sports1();
+        s.score_details(230);
+        s.score_details(278,217);
+        s.score_details(129,78,234);
+        System.out.println("");
+
+
+    }
+}
