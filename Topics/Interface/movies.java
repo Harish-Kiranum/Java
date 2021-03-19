dce0acd7d2e431b50338de2995cbc6faa8360490
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-19 09:06:11 +0530

    Interface program

--- Topics/Interface/movies.java
+++ Topics/Interface/movies.java
@@ -1,1 +1,33 @@
+interface movies_details{
+    public void movie_name(); // Abstract 
+    public void ratings(); 
+}
+class kolly_wood implements movies_details{
+    public void movie_name(){
+        System.out.println( "kolly wood movie name is"+" "+"Master");
+    }
+    public void ratings(){
+        System.out.println("movie rating"+" "+8);
+    }
 
+}
+class Holly_wood implements movies_details{
+    public void movie_name(){
+        System.out.println("Holly wood movie name is"+" "+"Tenet");
+    }
+    public void ratings(){
+        System.out.println("movie rating"+" "+7);
+    }
+}
+public class movies {
+    public static void main(String args[]){
+        Holly_wood h = new Holly_wood();
+        kolly_wood k = new kolly_wood();
+        h.ratings();
+        h.movie_name();
+        k.ratings();
+        k.movie_name();
+
+    }
+    
+}
