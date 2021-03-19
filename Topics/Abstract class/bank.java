ec9941c3d9a9a714ba34403dbbb282c605e29e84
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-19 09:03:53 +0530

    Abstract methods

--- Topics/Abstract class/bank.java
+++ Topics/Abstract class/bank.java
@@ -1,1 +1,36 @@
+ abstract class bank_details{
+     public void OTP_number(){// non- abstract method
+         System.out.println("send to your mobile phone");
 
+     }
+     abstract void amount();// abstract method ended with semicolon , no curly braces 
+}
+// inherits the abstract class
+class Indian_bank extends bank_details{ 
+    public void amount()
+    {
+        System.out.println(12000);
+    }
+    public void interest(){
+        System.out.println(345556);
+    }
+
+}
+class SBI_bank extends bank_details{
+    public void amount()
+    {
+        System.out.println(30000);
+    }
+}
+public class bank {
+    public static void main(String args[]){
+        Indian_bank b = new Indian_bank();
+        SBI_bank b1 = new SBI_bank();
+        b1.amount();
+        b.amount();
+        b.OTP_number();
+        b.interest();
+        System.out.println("");
+    }
+    
+}
