fd496619ccb31ac1946d32b17996251213c085a6
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-24 10:07:44 +0530

    Nested try

--- Topics/Topics/Exception handling/NestedTry.java
+++ Topics/Topics/Exception handling/NestedTry.java
@@ -1,1 +1,29 @@
 
+package Exceptionhandling;
+public class NestedTry {
+
+      public static void main(String[] args) {
+
+            try{
+                  System.out.println("Nesed try begins");
+
+                  try{
+                        int a = 47, b = 0;
+                        int div  = a/b;
+                  }catch(ArithmeticException e){
+                        System.out.println("Division by 0 not allowed");
+                  }
+
+                  try{
+                        int arr[] = {1,7,3,8,5};
+                        System.out.println(arr[10]);
+                  }catch(ArrayIndexOutOfBoundsException e){
+                        System.out.println("index doesnot exists in array");
+                  }
+
+            }catch(Exception e){
+                        System.out.println("Exception occured");
+            }
+
+      }
+}
