bff28589943b55ee87ce0c52644825e4bb87bfe2
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-26 09:42:06 +0530

    unchecked Exception

--- Topics/Fileconcepts/FileFolder.java
+++ Topics/Fileconcepts/FileFolder.java
@@ -1,1 +1,23 @@
 
+import java.io.File;
+import java.io.IOException;
+public class FileFolder {
+    public static void main(String args[]){
+        File file = new File("C:\\Users\\admin\\OneDrive\\Desktop\\Harish\\KCT");
+        try{
+
+        // Whether the file or directory is present or not
+            boolean present = file.exists();
+            System.out.println("Folder present :"+present);
+            if(present == false){
+                file.mkdir(); // folder creation
+                file.mkdirs(); // subfolder creation
+            present = file.exists();
+            System.out.println("Folder present: "+ present);
+            } 
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+}
