5465597cb6f05661947e160258744ccb41aa18f0
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-29 10:10:59 +0530

    Nested class

--- Topics/InnerClass/Faculty.java
+++ Topics/InnerClass/Faculty.java
@@ -1,1 +1,40 @@
+package innerclass;
+import java.util.*;
 
+class Staff{
+    private String name;
+    private String staffid;
+    // use parametrized constructor
+    public Staff(String iname, String istaffid){
+        name = iname;
+        staffid = istaffid;
+    }
+    // nested class
+    public class Degree{
+        public String degreename;
+        public int yearstarted;
+        // use parametrized constructors
+        public Degree(String idegreename, int iyearstarted){
+            degreename = idegreename;
+            yearstarted = iyearstarted;
+        }
+        public void display(){
+            System.out.println(" The staff name is"+name);
+            System.out.println("The staff id is"+ " "+staffid);
+            System.out.println(degreename+" "+yearstarted);
+        }
+    }
+}
+public class Faculty {
+    public static void main(String args[]){
+        // create an object for outer class
+        Staff s1 = new Staff("Harish", "17BCS089");
+        //  create an object for inner class
+        Staff.Degree dobj = s1.new Degree("BE",2017);
+        dobj.display();
+
+
+
+    }
+    
+}
