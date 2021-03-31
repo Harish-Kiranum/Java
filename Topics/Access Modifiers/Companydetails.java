7c97e81c7d3840dab139dcf1062838d70be75ff3
Harish-Kiranum <79856127+Harish-Kiranum@users.noreply.github.com>
2021-03-31 10:18:58 +0530

    Program on Access specifiers

--- Topics/Access Modifiers/Companydetails.java
+++ Topics/Access Modifiers/Companydetails.java
@@ -1,1 +1,48 @@
+package Access;
+class Employee{
+    // private modifier
+    private  int empid;
+    private  float Salary;
+    // getter method
+    public int getempid(){
+        return empid;
+    }
+    public void setempid(int empid){
+        this.empid = empid;
+    }
+    public float getSalary(){
+        return Salary;
+    }
+    public void setsalary(float Salary){
+        this.Salary = Salary;
+    }
+    // protected specifier
+    //It can be accessed by the in herited class
+    protected String company;
+    protected String name;
+    protected  Employee(String company, String name){
+        this.company = company;
+        this.name = name;
+        System.out.println(" ");
 
+    }
+    public void display(){
+        System.out.println("Enter the company name and employee name :"+" "+company+" "+name);
+    }
+}
+
+class Companydetails{
+     public static void main(String args[]){
+         Employee c1 = new Employee();
+         Employee c2 = new Employee("Kiranum","Harish");
+         c2.display();
+         c1.setempid(245678);
+         c1.setsalary(23400f);
+         System.out.println("The employee id is: "+" "+c1.getempid());
+         System.out.println("The employee salary is" +" "+c1.getSalary());
+
+
+
+    }
+
+}
